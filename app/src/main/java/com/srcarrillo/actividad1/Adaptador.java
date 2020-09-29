b3b807bde255ed12701748ee3701456fe86339ba
Thomas Carrillo <Thomascarrillogonzalez@gmail.com>
2020-09-29 20:23:20 -0300

    4 commit

--- app/src/main/java/com/srcarrillo/actividad1/Adaptador.java
+++ app/src/main/java/com/srcarrillo/actividad1/Adaptador.java
@@ -1,5 +1,6 @@
 package com.srcarrillo.actividad1;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,40 +13,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Adaptador extends RecyclerView.Adapter<Adaptador.ViewHolderRegistros> {
-    ArrayList<String> ListaRegistros;
+    Context context;
+    ArrayList<Persona> ListaPersonas;
 
-    public Adaptador(ArrayList<String> ListaRegistros){
-        ListaRegistros = ListaRegistros;
+    public Adaptador(Context context, ArrayList<Persona> ListaPersonas) {
+        this.context = context;
+        this.ListaPersonas = ListaPersonas;
     }
 
     @NonNull
     @Override
     public ViewHolderRegistros onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_simple,null,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_simple, null, false);
         return new ViewHolderRegistros(view);
     }
 
     @NonNull
     @Override
-    public void onBindViewHolder(@NonNull Adaptador.ViewHolderRegistros holder, int position){
-        holder.asignarDatos(ListaRegistros.get(position));
+    public void onBindViewHolder(@NonNull Adaptador.ViewHolderRegistros holder, int position) {
+        holder.RNombre.setText(ListaPersonas.get(position).getNombre());
+        holder.RCorreo.setText(ListaPersonas.get(position).getCorreo());
+        holder.RExp.setText(ListaPersonas.get(position).getExp());
+        holder.RAños.setText(ListaPersonas.get(position).getAños());
+        holder.RIntereses.setText(ListaPersonas.get(position).getIntereses());
     }
 
     @Override
     public int getItemCount() {
-        return ListaRegistros.size();
+        return ListaPersonas.size();
     }
-    public class ViewHolderRegistros extends RecyclerView.ViewHolder{
-        TextView Registrado;
 
-        public ViewHolderRegistros(@NonNull View itemView){
+    public class ViewHolderRegistros extends RecyclerView.ViewHolder {
+        TextView RNombre, RCorreo, RExp, RAños, RIntereses;
+
+        public ViewHolderRegistros(@NonNull View itemView) {
             super(itemView);
-            Registrado = itemView.findViewById(R.id.Registrado);
-
-
-        }
-        public void asignarDatos(String dato){
-            Registrado.setText(dato);
+            RNombre = itemView.findViewById(R.id.RNombre);
+            RCorreo = itemView.findViewById(R.id.RCorreo);
+            RExp = itemView.findViewById(R.id.RExp);
+            RAños = itemView.findViewById(R.id.RAños);
+            RIntereses = itemView.findViewById(R.id.RIntereses);
 
 
         }
---
+++ app/src/main/java/com/srcarrillo/actividad1/Persona.java
@@ -0,0 +1,57 @@
+package com.srcarrillo.actividad1;
+
+public class Persona {
+    String Nombre;
+    String Correo;
+    String Exp;
+    String Años;
+    String Intereses;
+
+    public Persona(String nombre, String correo, String exp, String años, String intereses) {
+        this.Nombre = Nombre;
+        this.Correo = Correo;
+        this.Exp = Exp;
+        this.Años = Años;
+        this.Intereses = Intereses;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.Correo = Correo;
+    }
+
+    public String getExp() {
+        return Exp;
+    }
+
+    public void setExp(String exp) {
+        this.Exp = Exp;
+    }
+
+    public String getAños() {
+        return Años;
+    }
+
+    public void setAños(String años) {
+        this.Años = Años;
+    }
+
+    public String getIntereses() {
+        return Intereses;
+    }
+
+    public void setIntereses(String intereses) {
+        this.Intereses = Intereses;
+    }
+}
